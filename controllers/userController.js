623216e0c1ab06322c19e6f6e87c3ee05dded015
Lucia <lumiraelmar@gmail.com>
2021-01-07 01:59:55 +0100

    drinks db

---
+++ controllers/userController.js
@@ -0,0 +1,22 @@
+class UserController {
+  constructor(userService, drinkService) {
+    this.userService = userService;
+    this.drinkService = drinkService;
+  }
+
+  async getUserByName(req, res) {
+    const { name } = req.params;
+    const user = await this.userService.getUserByName(name);
+    const drink = await this.drinkService.getDrinkByName(user.favDrink);
+
+    const data = {
+      name: user.name,
+      favDrink: user.favDrink,
+      mainIngredient: drink.mainIngredient,
+    };
+
+    res.json(data);
+  }
+}
+
+module.exports = UserController;
